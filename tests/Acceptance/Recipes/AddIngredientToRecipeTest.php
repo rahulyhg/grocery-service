397305beea3afc6df39e6fc4b89511af1c2725f3
Carwyn Nelson <bacondrinker@gmail.com>
2016-12-28 22:11:38 +0000

    Added initial contract test

---
+++ tests/Acceptance/Recipes/AddIngredientToRecipeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+class AddIngredientToRecipeTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldReturnAFullRecipeIngredientWhenTheCorrectDataIsPassed()
+    {
+        $response = $this->call(
+            'PUT',
+            '/recipe/1/ingredients',
+            [
+                'ingredient_id' => 1,
+                'amount' => '4 slices'
+            ]
+        );
+
+        $this->assertEquals(
+            (object) [
+                'code' => 'success',
+                'data' => (object) [
+                    'details' => (object) [
+                        'id' => 1,
+                        'name' => 'Bread'
+                    ],
+                    'amount' => '4 slices'
+                ]
+            ],
+            $response->getData()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnAValidationErrorIfTheIncorrectDetailsArePassed()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnAnErrorIfTheIngredientDoesNotExist()
+    {
+        
+    }
+}
